bc2de3d14edb1926accefaa1e01099000873473a
Kate <fukuro.s@yandex.ru>
2025-02-19 00:10:57 +0300

    reorganized structure
    - added 3 functions: get_branches, get_last_commit_id, get_obj_by_id
    - easier to read

--- 20250217/1/prog.py
+++ 20250217/1/prog.py
@@ -3,15 +3,25 @@ from glob import iglob
 from os.path import dirname, basename
 import sys
 
+def get_branches(path):
+    return '\n'.join([basename(i) for i in iglob(path + "/.git/refs/heads/*")])
+
+def get_last_commit_id(path, branch):
+    return open(path + '/.git/refs/heads/' + branch).read()
+
+def get_obj_by_id(path, obj_id):
+    f = open(path + f'/.git/objects/{obj_id[:2]}/{obj_id[2:-1]}', 'rb')
+    obj = decompress(f.read())
+    return obj.partition(b'\x00')
+
+
 path = sys.argv[1]
+
 if len(sys.argv) == 2:
-    for name in iglob(path + '/.git/refs/heads/*'):
-        print(basename(name))
+    print(get_branches(path))
 else:
     name = sys.argv[2]
-    last_commit = open(path + '/.git/refs/heads/' + name).read()
-    f = open(path + f'/.git/objects/{last_commit[:2]}/{last_commit[2:-1]}', 'rb')
-    obj = decompress(f.read())
-    header, _, body = obj.partition(b'\x00')
-    out = body.decode().replace('\n', '\n' + "  ")
-    print(f"  {out}")
+    last_commit_id = get_last_commit_id(path, name)
+    header, _, body = get_obj_by_id(path, last_commit_id)
+    print(body.decode())
+
