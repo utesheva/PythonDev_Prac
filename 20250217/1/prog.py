f7ab4c2422444a10c1bb05616781454a0ee26ca2
Kate <fukuro.s@yandex.ru>
2025-02-19 00:42:19 +0300

    print tree of last commit
    - added function parse_tree that returns tree in the right format

--- 20250217/1/prog.py
+++ 20250217/1/prog.py
@@ -7,13 +7,24 @@ def get_branches(path):
     return '\n'.join([basename(i) for i in iglob(path + "/.git/refs/heads/*")])
 
 def get_last_commit_id(path, branch):
-    return open(path + '/.git/refs/heads/' + branch).read()
+    return open(path + '/.git/refs/heads/' + branch).read()[:-1]
 
 def get_obj_by_id(path, obj_id):
-    f = open(path + f'/.git/objects/{obj_id[:2]}/{obj_id[2:-1]}', 'rb')
+    f = open(path + f'/.git/objects/{obj_id[:2]}/{obj_id[2:]}', 'rb')
     obj = decompress(f.read())
     return obj.partition(b'\x00')
 
+def parse_tree(path, body):
+    tree = []
+    tail = body
+    while tail:
+        treeobj, _, tail = tail.partition(b'\x00')
+        tmode, tname = treeobj.split()
+        num, tail = tail[:20], tail[20:]
+        obj_id = num.hex()
+        obj_name = get_obj_by_id(path, obj_id)[0].decode().split()[0]
+        tree.append([obj_name, obj_id, tname.decode()])
+    return tree
 
 path = sys.argv[1]
 
@@ -24,4 +35,7 @@ else:
     last_commit_id = get_last_commit_id(path, name)
     header, _, body = get_obj_by_id(path, last_commit_id)
     print(body.decode())
+    tree_id = body.decode().split('\n')[0].split()[1]
+    header, _, body = get_obj_by_id(path, tree_id)
+    print('\n'.join([' '.join(i) for i in parse_tree(path, body)]))
 
