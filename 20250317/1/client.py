f377e9f21f02240284ae444badffc77fa2469a74
Kate <fukuro.s@yandex.ru>
2025-03-20 20:06:43 +0300

    added sending commands after preprocessing

--- 20250317/1/client.py
+++ 20250317/1/client.py
@@ -57,38 +57,94 @@ def attack_check(self, x, y, args):
 class Client_MUD(cmd.Cmd):
     prompt = 'MUD> '
     readline.set_completer_delims(readline.get_completer_delims().replace('-', ''))
+    host = "localhost" if len(sys.argv) < 2 else sys.argv[1]
+    port = 1337 if len(sys.argv) < 3 else int(sys.argv[2])
 
+    def __init__(self, socket):
+        self.s = socket
+        self.s.connect((self.host, self.port))
+        return super().__init__()
+
+    def response(self):
+        print(self.s.recv(1024).rstrip().decode())
+    
     def do_addmon(self, args):
         try:
             x, y, hp, hello, name = add_monster_check(args)
+            self.s.sendall(f"addmon {name} {x} {y} {hp} {hello}".encode())
+            self.response()
         except Error as e:
             print(e.text)
 
     def do_attack(self, args):
         try:
             weapon, name = attack_check(args)
+            self.s.sendall(f"attack {weapon} {name}".encode())
+            self.response()
         except Error as e:
             print(e.text)
     
     def do_up(self, args):
         if len(args) != 0:
             print(Error(1).text)
+        else:
+            self.s.sendall(f"move 0 -1".encode())
+            self.response()
 
     def do_down(self, args):
         if len(args) != 0:
             print(Error(1).text)
+        else:
+            self.s.sendall(f"move 0 1".encode())
+            self.response()
 
     def do_left(self, args):
         if len(args) != 0:
             print(Error(1).text)
+        else:
+            self.s.sendall(f"move -1 0".encode())
+            self.response()
 
     def do_right(self, args):
         if len(args) != 0:
             print(Error(1).text)
+        else:
+            self.s.sendall(f"move 1 0".encode())
+            self.response()
+    
+    def default(self, args):
+        print("Invalid command")
+
+    def complete_addmon(self, text, line, begidx, endidx):
+        words = (line[:endidx] + ".").split()
+        DICT = list({'hello', 'hp', 'coords'} - set(line[:endidx].split()))
+        if 'coords' in words and words[-2] != 'coords':
+            condition = (len(words) % 2 == 0)
+        else:
+            condition = (len(words) % 2 == 1)
+        if len(words) == 2:
+            DICT = cows
+        elif not condition:
+            DICT = []
+        return [c for c in DICT if c.startswith(text)]
+
+    def complete_attack(self, text, line, begidx, endidx):
+        words = (line[:endidx] + ".").split()
+        if (len(words) == 2 and
+            (not hasattr(self, 'ind') or
+             hasattr(self, 'ind') and self.matches[self.ind] != text)):
+            self.matches = [c for c in self.game.cows if c.startswith(text)]
+            self.ind = -1
+        elif len(words) == 3:
+            self.matches = ['with']
+            self.ind = -1
+        elif len(words) == 4 and words[-2] == 'with' and (self.matches[self.ind] != text or text == ''):
+            self.matches = ['sword', 'spear', 'axe']
+            self.ind = -1
+        self.ind = (self.ind + 1) % len(self.matches)
+        return [self.matches[self.ind]]
 
 if __name__ == '__main__':
-    host = "localhost" if len(sys.argv) < 2 else sys.argv[1]
-    port = 1337 if len(sys.argv) < 3 else int(sys.argv[2])
-    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-        s.connect((host, port))
-        Client_MUD().cmdloop()
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    Client_MUD(socket=s).cmdloop()
