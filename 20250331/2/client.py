3c4d9d103006edca85bab043a680713b9d152aa4
Kate <fukuro.s@yandex.ru>
2025-04-05 19:12:05 +0300

    copy of the task 1

---
+++ 20250331/2/client.py
@@ -0,0 +1,92 @@
+import cmd
+import readline
+import sys
+import socket
+import threading
+import cowsay
+import shlex
+
+cows = cowsay.list_cows() + ['jgsbat']
+
+class Client_MUD(cmd.Cmd):
+    prompt = 'MUD> '
+    readline.set_completer_delims(readline.get_completer_delims().replace('-', ''))
+
+    def __init__(self, *args, socket, **kwargs):
+        self.s = socket
+        return super().__init__(*args, **kwargs)
+       
+    def do_addmon(self, args):
+        self.s.sendall(f"addmon {args}\n".encode())
+
+    def do_attack(self, args):
+        self.s.sendall(f"attack {args}\n".encode())
+    
+    def do_up(self, args):
+        self.s.sendall(f"move 0 -1\n".encode())
+
+    def do_down(self, args):
+        self.s.sendall(f"move 0 1\n".encode())
+
+    def do_left(self, args):
+        self.s.sendall(f"move -1 0\n".encode())
+
+    def do_right(self, args):
+        self.s.sendall(f"move 1 0\n".encode())
+
+    def do_sendall(self, args):
+        self.s.sendall(f"sendall {args}\n".encode())
+
+    def default(self, args):
+        print("Invalid command")
+
+    def complete_addmon(self, text, line, begidx, endidx):
+        words = shlex.split(line[:endidx] + ".")
+        DICT = list({'hello', 'hp', 'coords'} - set(line[:endidx].split()))
+        if 'coords' in words and words[-2] != 'coords':
+            condition = (len(words) % 2 == 0)
+        else:
+            condition = (len(words) % 2 == 1)
+        if len(words) == 2:
+            DICT = cows
+        elif not condition:
+            DICT = []
+        return [c for c in DICT if c.startswith(text)]
+
+    def complete_attack(self, text, line, begidx, endidx):
+        words = (line[:endidx] + ".").split()
+        if (len(words) == 2 and
+            (not hasattr(self, 'ind') or
+             hasattr(self, 'ind') and self.matches[self.ind] != text)):
+            self.matches = [c for c in cows if c.startswith(text)]
+            self.ind = -1
+        elif len(words) == 3:
+            self.matches = ['with']
+            self.ind = -1
+        elif len(words) == 4 and words[-2] == 'with' and (self.matches[self.ind] != text or text == ''):
+            self.matches = ['sword', 'spear', 'axe']
+            self.ind = -1
+        self.ind = (self.ind + 1) % len(self.matches)
+        return [self.matches[self.ind]]
+
+    def from_srv(self, cmdline, s):
+        while response := s.recv(1024).rstrip().decode():
+            print(f"\n{response}\n{cmdline.prompt}{readline.get_line_buffer()}", end="", flush=True)
+
+if __name__ == '__main__':
+    host = "localhost" if len(sys.argv) < 3 else sys.argv[2]
+    port = 1337 if len(sys.argv) < 4 else int(sys.argv[3])
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.connect((host, port))
+    s.sendall(f"{sys.argv[1]}\n".encode())
+    if s.recv(1024).rstrip().decode() == '1':
+        print("<<< Welcome to Python-MUD 0.1 >>>")
+        print(f"Your login: {sys.argv[1]}")
+        cmdline = Client_MUD(socket=s)
+        mes = threading.Thread(target=cmdline.from_srv, args=(cmdline, s))
+        mes.start()
+        cmdline.cmdloop()
+    else:
+        print("ERROR: Choose another login")
+    s.shutdown(socket.SHUT_RDWR)
---
+++ 20250331/2/server.py
@@ -0,0 +1,220 @@
+import sys
+import asyncio
+import cowsay
+from io import StringIO
+import shlex
+
+COWS = cowsay.list_cows() + ['jgsbat']
+
+JGSBAT = cowsay.read_dot_cow(StringIO(r"""
+    ,_                    _,
+    ) '-._  ,_    _,  _.-' (
+    )  _.-'.|\\\--//|.'-._  (
+     )'   .'\/o\/o\/'.   `(
+      ) .' . \====/ . '. (
+       )  / <<    >> \  (
+        '-._/``  ``\_.-'
+  jgs    __\\\'--'//__
+         (((""`  `"")))
+"""))
+
+class Error(BaseException):
+    def __init__(self, code, name = ''):
+        match code:
+            case 1:
+                self.text = "Invalid arguments"
+            case 2:
+                self.text = "Cannot add unknown monster"
+            case 3:
+                self.text = f"No {name} here"
+            case 4:
+                self.text = "Unknown weapon"
+
+
+class Player:
+    def __init__(self):
+        self.x, self.y = 0, 0
+        self.queue = asyncio.Queue()
+
+    def move(self, d_x, d_y):
+        self.x = (self.x + d_x) % 10
+        self.y = (self.y + d_y) % 10
+        return f"Moved to ({self.x}, {self.y})\n"
+
+
+class Monster:
+    def __init__(self, x, y, name, phrase, hitpoints):
+        self.x = x
+        self.y = y
+        self.phrase = phrase
+        self.cow = name
+        self.hp = hitpoints
+
+    def say(self):
+        if self.cow == 'jgsbat':
+            return cowsay.cowsay(self.phrase, cowfile=jgsbat)
+        else:
+            return cowsay.cowsay(self.phrase, cow=self.cow)
+
+class Game:
+    def __init__(self):
+        self.size = 10
+        self.monsters = {}
+
+    def encounter(self, x, y):
+        if self.monsters[(x, y)]:
+            return self.monsters[(x, y)].say()
+        return ''
+
+    def moving(self, player, d_x, d_y):
+        s = player.move(d_x, d_y)
+        if (player.x, player.y) in self.monsters:
+            s += self.encounter(player.x, player.y)
+        return s
+
+    def add_monster(self, x, y, hp, hello, name):
+        ans = f"Added monster {name} to ({x}, {y}) saying {hello}\n"
+        if (x,y) in self.monsters and not(self.monsters[(x,y)] is None):
+            ans += "Replaced the old monster\n"
+        self.monsters[(x, y)] = Monster(x, y, name, hello, hp)
+        return ans
+
+    def attack(self, x, y, weapon, name):
+        if ((x, y) not in self.monsters or
+            self.monsters[(x, y)] is None or
+            self.monsters[(x, y)].cow != name):
+            raise Error(3, name)
+        damage = min(self.monsters[(x, y)].hp, weapon)
+        self.monsters[(x, y)].hp = self.monsters[(x, y)].hp - damage
+        ans = f"attacked {self.monsters[(x, y)].cow}, damage {damage} hp\n"
+        if self.monsters[(x, y)].hp == 0:
+            ans += f"{self.monsters[(x, y)].cow} died\n"
+            self.monsters[(x, y)] = None
+        else:
+            ans += f"{self.monsters[(x, y)].cow} now has {self.monsters[(x, y)].hp}\n"
+        return ans
+
+
+def parse_args(args, param):
+    args_parsed = {}
+    for i in param:
+        if i not in args:
+            return None
+        args_parsed[i] = args[args.index(i) + 1: args.index(i) + 1 + param[i]]
+    return args_parsed
+
+def add_monster_check(args):
+    preprocess = shlex.split(args)
+    if len(preprocess) != 8:
+        raise Error(1)
+    name = preprocess[0]
+    parsed_args = parse_args(preprocess[1:], {"hello": 1, "hp": 1, "coords": 2})
+    if not parsed_args:
+        raise Error(1)
+    x, y = parsed_args['coords']
+    hello = parsed_args['hello'][0]
+    hp = parsed_args['hp'][0]
+    if (not x.isdigit() or
+        not y.isdigit() or
+        not hp.isdigit()):
+        raise Error(1)
+    x, y, hp = map(int, [x, y, hp])
+    if x < 0 or x >= 10 or y < 0 or y >= 10 or hp <= 0:
+        raise Error(1)
+    if name not in COWS:
+        raise Error(2)
+    return x, y, hp, hello, name
+
+def attack_check(args):
+    splitted = shlex.split(args)
+    parsed_args = parse_args(splitted, {'with': 1})
+    if parsed_args:
+        match parsed_args['with'][0]:
+            case 'sword': weapon = 10
+            case 'spear': weapon = 15
+            case 'axe': weapon = 20
+            case _:
+                raise Error(4)
+    else: weapon = 10
+    if len(args) == 0 or splitted[0] not in COWS:
+        raise Error(1)
+    name = splitted[0]
+    return weapon, name
+
+async def send_all(mes, exception=None):
+    for out in players.values():
+        if out != exception:
+            await out.queue.put(f"{mes}")
+
+async def echo(reader, writer):
+    global game, players
+
+    send = asyncio.create_task(reader.readline())
+
+    await asyncio.wait_for(send, timeout=None)
+    login = send.result().decode()[:-1]
+    if login in players:
+        writer.write('0'.encode())
+        writer.close()
+        send.cancel()
+        await writer.wait_closed()
+        return
+    else:
+        players[login] = Player()
+        receive = asyncio.create_task(players[login].queue.get())
+        writer.write(f"1".encode())
+        await send_all(f'New player: {login}', exception=players[login])
+
+
+    me = "{}:{}".format(*writer.get_extra_info('peername'))
+    print(login, me)
+
+    while not reader.at_eof():
+        done, pending = await asyncio.wait([send, receive], return_when=asyncio.FIRST_COMPLETED)
+        for request in done:
+            if request is send:
+                send = asyncio.create_task(reader.readline())
+                match request.result().decode().split(maxsplit=1):
+                    case ['addmon', args]:
+                        try:
+                            x, y, hp, hello, name = add_monster_check(args)
+                        except Error as e:
+                            writer.write(e.text.encode())
+                            continue
+                        await send_all(game.add_monster(int(x), int(y), int(hp), hello, name))
+                    case ['attack', args]:
+                        try:
+                            x, y = players[login].x, players[login].y 
+                            weapon, name = attack_check(args)
+                            await send_all(f'{login} {game.attack(x, y, int(weapon), name)}')
+                        except Error as e:
+                            writer.write(e.text.encode())
+                            continue         
+                    case ['move', args]:
+                        d_x, d_y = [int(i) for i in args.split()]
+                        writer.write(game.moving(players[login], d_x, d_y).encode())
+                    case ['sendall', args]:
+                        args = shlex.split(args)[0]
+                        await send_all(f"{login}: {args}", exception=players[login])
+            if request is receive:
+                receive = asyncio.create_task(players[login].queue.get())
+                writer.write(f"{request.result()}\n".encode())
+                await writer.drain()
+
+    send.cancel()
+    receive.cancel()
+    writer.close()
+    print(login, "LEFT")
+    del players[login]
+    await send_all(f"{login} left")
+    await writer.wait_closed()
+
+async def main():
+    global game, players
+    game = Game()
+    players = {}
+    server = await asyncio.start_server(echo, '0.0.0.0', 1337)
+    async with server:
+        await server.serve_forever()
+
+asyncio.run(main())
