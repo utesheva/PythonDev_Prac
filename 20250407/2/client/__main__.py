4296d9ffac6148ab99664ffba7751c14b2288e11
Kate <fukuro.s@yandex.ru>
2025-04-13 23:24:11 +0300

    added scripting

--- 20250407/2/client/__main__.py
+++ 20250407/2/client/__main__.py
@@ -5,6 +5,7 @@ import socket
 import threading
 import cowsay
 import shlex
+import time
 
 cows = cowsay.list_cows() + ['jgsbat']
 
@@ -22,6 +23,10 @@ class Client_MUD(cmd.Cmd):
         """
         self.s = socket
         return super().__init__(*args, **kwargs)
+
+    def precmd(self, line):
+        time.sleep(1)
+        return super().precmd(line)
 
     def do_addmon(self, args):
         """
@@ -73,6 +78,11 @@ class Client_MUD(cmd.Cmd):
         """
         self.s.sendall(f"sendall {args}\n".encode())
 
+    def do_EOF(self, args):
+        return 1
+    def emptyline(self):
+        return 
+
     def default(self, args):
         """Process any other commands"""
         print("Invalid command")
@@ -120,8 +130,12 @@ class Client_MUD(cmd.Cmd):
 
 
 if __name__ == '__main__':
-    host = "localhost" if len(sys.argv) < 3 else sys.argv[2]
-    port = 1337 if len(sys.argv) < 4 else int(sys.argv[3])
+    if sys.argv[2] == '--file' and len(sys.argv) > 3:
+        file = open(sys.argv[3])
+    else:
+        file = None
+    host = "localhost"
+    port = 1337
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     s.connect((host, port))
@@ -129,10 +143,17 @@ if __name__ == '__main__':
     if s.recv(1024).rstrip().decode() == '1':
         print("<<< Welcome to Python-MUD 0.1 >>>")
         print(f"Your login: {sys.argv[1]}")
-        cmdline = Client_MUD(socket=s)
+        if file is None:
+            cmdline = Client_MUD(socket=s)
+        else:
+            cmdline = Client_MUD(socket=s, stdin=file)
+            cmdline.prompt = ''
+            cmdline.use_rawinput = False
         mes = threading.Thread(target=cmdline.from_srv, args=(cmdline, s))
         mes.start()
         cmdline.cmdloop()
     else:
         print("ERROR: Choose another login")
     s.shutdown(socket.SHUT_RDWR)
+    if file:
+        file.close()
