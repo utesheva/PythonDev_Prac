8107e9e40c0ee6ac614506a03094ae3a57675863
Kate <fukuro.s@yandex.ru>
2025-04-28 14:04:52 +0300

    added stat

--- 20250428/0/dodo.py
+++ 20250428/0/dodo.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from zipfile import ZipFile
 
 DOIT_CONFIG = {"default_tasks": ['docs']}
 
@@ -13,7 +14,22 @@ def task_zip():
     """Zip docs"""
     return {
             "task_dep": ["docs"],
+            "targets": ["docs.zip"],
             "actions": ["zip -r docs.zip _build/html"]
+    }
+
+def task_stat():
+    """List zip archive"""
+    def zip_smth(zipfile, targets):
+        with ZipFile(zipfile, "r") as zf:
+            f = '\n'.join(zf.namelist())
+            with open(targets[0], "w") as output:
+                output.write(f)
+
+    return {
+            "actions": [(zip_smth, ["docs.zip"])],
+            "file_dep": ["docs.zip"],
+            "targets": ["docs.list"]
     }
 
 def task_erase():
@@ -21,3 +37,4 @@ def task_erase():
     return {
             "actions": ["git reset --hard", "git clean -xdf"]
     }
+
